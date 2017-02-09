e1176dacffa04cf5434af1a9cbfdf5758e0f6ad9
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-02-09 08:45:18 -0600

    Populated popup

--- scripts/app.js
+++ scripts/app.js
@@ -9,13 +9,39 @@ angular
         })
       }
       $scope.popInfo = (selection) => {
-            // console.log(selection)
-            $mdDialog.show(
-                $mdDialog.alert()
-                .clickOutsideToClose(true)
-                .title(selection.name)
-                .htmlContent(`${selection.birth_year}`)
-                //.ok('CHEERS')
-            );
+            const films = selection.films;
+            function getFilmInfo(film) {
+              return new Promise((resolve, reject) => {
+                $http.get(film)
+                .success((data) => {
+                  resolve(`<a href="#">${data.title}</a>`);
+                })
+              })
+            }
+            const mappedFilms = films.map(film => getFilmInfo(film))
+            Promise.all(mappedFilms)
+            .then((results) => {
+              $mdDialog.show(
+                  $mdDialog.alert()
+                  .clickOutsideToClose(true)
+                  .title(selection.name)
+                  .htmlContent(`
+                    <div class="row">
+                      <div class="col-xs-5">
+                        Birth Year: ${selection.birth_year}
+                        <br />Gender: ${selection.gender}
+                        <br />Eye Color: ${selection.eye_color}
+                        <br />Hair Color: ${selection.hair_color}
+                        <br />Height: ${selection.height}
+                        <br />Mass: ${selection.mass}
+                      </div>
+                      <div class="col-xs-7">
+                        Movies:
+                        <br />${results}
+                        <br />
+                      </div>
+                    </div>`)
+              );
+            })
         }
     })
