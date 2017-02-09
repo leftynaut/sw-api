b2fe84e5596113ea3404ffabe8ce34c4f7135272
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-02-09 09:30:29 -0600

    Film popup

--- scripts/app.js
+++ scripts/app.js
@@ -23,10 +23,11 @@ angular
           $http.get(`https://swapi.co/api/films/?search=${$scope.query}`)
           .success((data) => {
             let films = data.results;
-            console.log(films)
             films = films.map(item => {
               return {
-                name: item.title
+                name: item.title,
+                opening_crawl: item.opening_crawl,
+                characters: item.characters
               }
             })
             $scope.data = films
@@ -34,39 +35,71 @@ angular
         }
       }
       $scope.popInfo = (selection) => {
-            const films = selection.films;
-            function getFilmInfo(film) {
-              return new Promise((resolve, reject) => {
-                $http.get(film)
-                .success((data) => {
-                  resolve(`<a href="#">${data.title}</a>`);
-                })
-              })
-            }
-            const mappedFilms = films.map(film => getFilmInfo(film))
-            Promise.all(mappedFilms)
-            .then((results) => {
-              $mdDialog.show(
-                  $mdDialog.alert()
-                  .clickOutsideToClose(true)
-                  .title(selection.name)
-                  .htmlContent(`
-                    <div class="row">
-                      <div class="col-xs-5">
-                        Birth Year: ${selection.birth_year}
-                        <br />Gender: ${selection.gender}
-                        <br />Eye Color: ${selection.eye_color}
-                        <br />Hair Color: ${selection.hair_color}
-                        <br />Height: ${selection.height}
-                        <br />Mass: ${selection.mass}
-                      </div>
-                      <div class="col-xs-7">
-                        Movies:
-                        <br />${results}
-                        <br />
-                      </div>
-                    </div>`)
-              );
+        if (!selection.characters) {
+        const films = selection.films;
+        function getFilmInfo(film) {
+          return new Promise((resolve, reject) => {
+            $http.get(film)
+            .success((data) => {
+              resolve(`<a href="#">${data.title}</a>`);
             })
+          })
         }
+        const mappedFilms = films.map(film => getFilmInfo(film))
+        Promise.all(mappedFilms)
+        .then((results) => {
+          $mdDialog.show(
+              $mdDialog.alert()
+              .clickOutsideToClose(true)
+              .title(selection.name)
+              .htmlContent(`
+                <div class="row">
+                  <div class="col-xs-5">
+                    Birth Year: ${selection.birth_year}
+                    <br />Gender: ${selection.gender}
+                    <br />Eye Color: ${selection.eye_color}
+                    <br />Hair Color: ${selection.hair_color}
+                    <br />Height: ${selection.height}
+                    <br />Mass: ${selection.mass}
+                  </div>
+                  <div class="col-xs-7">
+                    Movies:
+                    <br />${results}
+                    <br />
+                  </div>
+                </div>`)
+          );
+        })
+      } else {
+        const characters = selection.characters;
+        function getCharacterInfo(character) {
+          return new Promise((resolve, reject) => {
+            $http.get(character)
+            .success((data) => {
+              resolve(`<a href="#">${data.name}</a>`);
+            })
+          })
+        }
+        const mappedCharacters = characters.map(character => getCharacterInfo(character))
+        Promise.all(mappedCharacters)
+        .then((results) => {
+          $mdDialog.show(
+              $mdDialog.alert()
+              .clickOutsideToClose(true)
+              .title(selection.name)
+              .htmlContent(`
+                <div class="row">
+                  <div class="col-xs-5">
+                    Opening Crawl:<br />${selection.opening_crawl}
+                  </div>
+                  <div class="col-xs-7">
+                    Characters:
+                    <br />${results}
+                    <br />
+                  </div>
+                </div>`)
+          );
+        })
+      }
+      }
     })
