2e4d01f4a2e30848c8a461038b865c8f0e31fb04
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-02-08 21:52:06 -0600

    Back to MVP again

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-import Searchbox from './searchbox/searchbox';
-
-const module = angular.module('components.module', [])
-	.component('searchbox', Searchbox)
-	.name;
-
-export default module;
